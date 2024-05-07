65c0b93515ca17b5076e0186c6e3b0a0639241ee
jhc <740535927@qq.com>
2024-05-07 18:31:00 +0800

    更改了next数组的算法，填补了在特殊情况下的缺陷，同时更改了由于角标在index函数的缺陷

--- 源.cpp
+++ 源.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-void get_next(string& T, int next[],int len_T)
+void get_next(string& T, int next[], int len_T)
 {
 	next[0] = 0;
 	int i = -1;
@@ -11,22 +11,42 @@ void get_next(string& T, int next[],int len_T)
 		{
 			j++;
 			i++;
-			next[j] = i+1;
+			next[j] = i + 1;
 		}
 		else {
 			i = next[i] - 1;
 
 		}
-    }
+	}
 }
-void show_next(int next[], int len)
+void show_next(int nextval[], int len)
 {
 	for (int i = 0; i < len; i++)
-		cout << next[i] << " ";
+		cout << nextval[i] << " ";
+}
+void get_nextval(string& T, int nextval[], int len_T)
+{
+	nextval[0] = 0;
+	int j = -1;
+	int i = 0;
+	while (i < len_T)
+	{
+		if (j == -1 || T[i] == T[j])
+		{
+			i++;
+			j++;
+			if (T[i] == T[j]) nextval[i] = nextval[j];
+			else nextval[i] = j + 1;
+		}
+		else {
+			j = nextval[j] - 1;
+		}
+	}
 }
 int index(int i, int next[], string s, string T, int len_T, int len_s)
 {
 	int j = -1;
+	i--;
 	while (j < len_T && i < len_s)
 	{
 		if (j == -1 || s[i] == T[j])
@@ -38,7 +58,7 @@ int index(int i, int next[], string s, string T, int len_T, int len_s)
 			j = next[j] - 1;
 		}
 	}
-	
+
 	return j == len_T ? i - len_T + 1 : 0;
 }
 int main()
@@ -49,8 +69,8 @@ int main()
 	cin >> T;
 	int len_T = T.size();
 	int len_s = s.size();
-	int* next = new int[len_T+1];
-	get_next(T, next, len_T);
-		//show_next(next, len_T);
+	int* next = new int[len_T + 1];
+	get_nextval(T, next, len_T);
+	//show_next(next, len_T);
 	cout << index(0, next, s, T, len_T, len_s) << endl;
 }
