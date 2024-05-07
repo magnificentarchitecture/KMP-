73d3bb90c7e08496a58cbb1337177301c5776902
jhc <740535927@qq.com>
2024-05-07 18:04:56 +0800

    add README

--- 源.cpp
+++ 源.cpp
@@ -24,15 +24,33 @@ void show_next(int next[], int len)
 	for (int i = 0; i < len; i++)
 		cout << next[i] << " ";
 }
+int index(int i, int next[], string s, string T, int len_T, int len_s)
+{
+	int j = -1;
+	while (j < len_T && i < len_s)
+	{
+		if (j == -1 || s[i] == T[j])
+		{
+			i++;
+			j++;
+		}
+		else {
+			j = next[j] - 1;
+		}
+	}
+	
+	return j == len_T ? i - len_T + 1 : 0;
+}
 int main()
 {
 	string s;
 	string T;//Ä£°æ
-	//cin >> s;
+	cin >> s;
 	cin >> T;
 	int len_T = T.size();
 	int len_s = s.size();
 	int* next = new int[len_T+1];
-	get_next(T, next,len_T);
-	show_next(next, len_T);
+	get_next(T, next, len_T);
+		//show_next(next, len_T);
+	cout << index(0, next, s, T, len_T, len_s) << endl;
 }
